a13a47470c02426b40563f4c2b115e6dadaf9071
Bernix <selvasekarbernix@gmail.com>
2022-03-01 03:12:24 +0530

    Added Some Logics

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,15 @@
+import React, { useState } from 'react'
+
+export default function Selection(props) {
+    const [style, setStyle] = useState({background: ''});
+    console.log(props)
+    const {applyColor} = props;
+  return (
+      <div className='fix-box' onClick={()=>{
+          applyColor(setStyle)
+      }} style={style}>
+          <h2 className='subheading' >Selection</h2>
+      </div> 
+  )
+}
 
